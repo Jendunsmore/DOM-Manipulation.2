173a83e26b5ca5b72f96a161f55bc009c41b2107
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-09-22 19:08:43 -0400

    ABC

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,24 @@
 
 // Menu Link Data
-var menuLinks = [
+var menuLinks = [ //---------- DOM 2 | Part 4 ---------------//
     { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
+    { text: 'catalog', href: '#', subLinks: [
+        {text: 'all', href: 'catalog/all'},
+        {text: 'top selling', href: '/catalog/top'},
+        {text: 'search', href: '/catalog/search'},
+    ]},
+    { text: 'orders', href: '#', subLinks: [
+        {text: 'new', href: '/orders/new'},
+        {text: 'pending', href: '/orders/pending'},
+        {text: 'history', href: '/orders/history'},
+    ]},
+    { text: 'account', href: '#', subLinks:[
+        {text: 'profile', href: '/account/profile'},
+        {text: 'sign out', href: '/account/signout'},
+    ]},
 ];
+//---------------------------------------------------------------------------------//
+
 
 //----------------------------- DOM 1 | Part 1 ------------------------------------//
 
@@ -21,7 +34,7 @@ mainEl[0].innerHTML = '<h1>DOM Manipulation</h1>';
 
 // 4. Add a class of flex-ctr to mainEl. Hint: Use the Element.classList API.
 mainEl[0].classList.add('flex-ctr');
-
+//----------------------------------------------------------------------------------//
 
 //----------------------------- DOM 1 | Part 2 ------------------------------------//
 
@@ -37,6 +50,20 @@ topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 // 4. Add a class of flex-around to topMenuEl.
 topMenuEl.classList.add('flex-around');
 
+//----------------------------- DOM 2 | Part 4 -------------------------------------//
+// 1. Select and cache the all of the <a> elements inside of topMenuEl in a variable named topMenuLinks.
+let topMenuLinks = topMenuEl.querySelectorAll('a');
+
+// 2. Attach a delegated 'click' event listener to topMenuEl.
+
+
+    // 1. The first line of code of the event listener function should call the event object's preventDefault() method.
+
+
+    // 2. The second line of code of the function should immediately return if the element clicked was not an <a> element.
+
+
+    // 3. Log the content of the <a> to verify the handler is working.
 
 //----------------------------- DOM 1 | Part 3 ------------------------------------//
 
@@ -56,6 +83,7 @@ menuLinks.forEach((Link) => {
     // 5. Append the new element to the topMenuEl element.
     topMenuEl.appendChild(newLink);
 })
+//----------------------------------------------------------------------------------//
 
 
 //----------------------------- DOM 2 | Part 3 ------------------------------------//
@@ -70,7 +98,6 @@ subMenuEl.style.height = '100%';
 // CSS custom property.
 subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
 
-
 // 4. Add the class of flex-around to the subMenuEl element.
 subMenuEl.classList.add('flex-around');
 
@@ -79,3 +106,6 @@ subMenuEl.style.position = 'absolute';
 
 // 6. Set the CSS top property of subMenuEl to the value of 0.
 subMenuEl.style.top = '0';
+
+
+//----------------------------------------------------------------------------------//
